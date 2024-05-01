071f94c100a830be4e26233d6e0d77319a6a1708
Juan Fernandes <juanzitow2@gmail.com>
2024-05-01 08:37:48 -0300

    feat: added integration to list years with multiple winners projection

--- screens/dashboard-screen/index.tsx
+++ screens/dashboard-screen/index.tsx
@@ -1,28 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import useMovies from "../../api/use-movies";
+import { ScrollView, StyleSheet, View } from "react-native";
+import YearsWithMultipleWinnersSection from "./years-with-multiple-winners-section";
 
-type DashboardScreenProps = {};
-
-export default function DashboardScreen(props: DashboardScreenProps) {
-  const { data, error, isLoading } = useMovies({
-    page: 1,
-    size: 10,
-  });
-
+export default function DashboardScreen() {
   return (
     <ScrollView>
+      <StatusBar style="auto" />
       <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text>Dashboard!!</Text>
-        {Boolean(error) && (
-          <>
-            <Text>Error fetching data:</Text>
-            <Text>{JSON.stringify(error, null, 2)}</Text>
-          </>
-        )}
-        {isLoading && <Text>Loading...</Text>}
-        {Boolean(data) && <Text>{JSON.stringify(data, null, 2)}</Text>}
+        <YearsWithMultipleWinnersSection />
       </View>
     </ScrollView>
   );
@@ -31,8 +16,7 @@ export default function DashboardScreen(props: DashboardScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    gap: 24,
+    padding: 16,
   },
 });
---
+++ screens/dashboard-screen/years-with-multiple-winners-section.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import useYearsWithMultipleWinners from "../../api/use-years-with-multiple-winners";
+
+export default function YearsWithMultipleWinnersSection() {
+  const { data, error, isLoading } = useYearsWithMultipleWinners();
+  return (
+    <View style={styles.container}>
+      <Text>Years with multiple winners</Text>
+      {data?.map?.((item) => (
+        <View key={item?.year} style={{ flexDirection: "row", gap: 12 }}>
+          <Text>{item?.year}</Text>
+          <Text>{item?.winnerCount}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { height: 80, width: "100%", padding: 8 },
+});
